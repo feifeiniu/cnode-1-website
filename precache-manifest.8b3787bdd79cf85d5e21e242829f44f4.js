a3aa71a37b9d134aa1c66ff0b484fee7162b1140
jingfei <987003803@qq.com>
2021-09-24 18:07:30 +0800

    x

---
+++ precache-manifest.8b3787bdd79cf85d5e21e242829f44f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ccb7cd2f3ac6b7db01",
+    "url": "/css/app.91665320.css"
+  },
+  {
+    "revision": "3618d45c2f4337547292ca6f061c9423",
+    "url": "/img/1.3618d45c.gif"
+  },
+  {
+    "revision": "c9c1d6a8c9cd4e7d51603427d7e49243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44ccb7cd2f3ac6b7db01",
+    "url": "/js/app.88de5e67.js"
+  },
+  {
+    "revision": "345d5f4c1dc11cff40a3",
+    "url": "/js/chunk-vendors.602e12c3.js"
+  },
+  {
+    "revision": "270c0e7b87b7a8313f311925eefb51ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
